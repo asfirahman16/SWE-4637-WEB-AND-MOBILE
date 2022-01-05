14f4fa4183e7f801052b9ed98451506c8566b3dc
asfi_rahman <asfirahman@iut-dhaka.edu>
2022-01-06 01:54:06 +0600

    Add vote count and like button and vote is incrementing and add Book.css

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,15 +1,27 @@
+import "./Book.css";
+
 function Book(props){
-    //const img_url="https://m.media-amazon.com/images/I/511-vIg1HaL._AC_UY327_FMwebp_QL65_.jpg";
-    //const name="Harry potter";
-    //const desc="This is a fictional book";
-    //const price=12;
+    let vote_count = props.votes;
+    console.log(props);
+    
+    function likeHandler(e){
+        console.log("clicked " + props.name);
+        //props.votes++;
+        vote_count++;
+        console.log(vote_count);
+    }
 
     return(
         <div className="book"> 
             <img src={props.img_url} alt=""/>
             <h2>Name: {props.name}</h2>
             <p>description: {props.desc}</p>
-            <span>price: {props.price}</span>
+            <span>price: {props.price}</span><br/>
+            <span className="vote_count">Votes: {vote_count}</span>
+            <div>
+                <img onClick={likeHandler} src="assets/upvote.jpg" alt="" className="upvote_img"/>
+            </div>
+            {/*<p>{props.children}</p>*/}
         </div>
     )
 }
--- src/BookList.jsx
+++ src/BookList.jsx
@@ -3,30 +3,45 @@ import Book from './Book';
 
 const book_lib = [
     {
+        id:1,
         name: "React",
         img_url: "https://m.media-amazon.com/images/I/511-vIg1HaL._AC_UY327_FMwebp_QL65_.jpg",
         desc: "This is an Intro to react",
-        price: 49
+        price: 49,
+        votes:12
     },
     {
+        id:2,
         name: "Computer Vision",
         img_url: "https://m.media-amazon.com/images/I/61TKTkmD4FL._AC_UY327_FMwebp_QL65_.jpg",
         desc: "This is an Intro to Computer vision",
-        price: 100
+        price: 100,
+        votes:19
     },
     {
+        id:3,
         name: "Will",
         img_url: "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg",
         desc: "This is a personal book",
-        price: 49
+        price: 49,
+        votes:22
     }
 ]
 
 function BookList(){
 
-    const newbooklist = book_lib.map(
-    bk => <Book img_url={bk.img_url} name={bk.name} desc={bk.desc} price={bk.price}/>
-    );
+    const newbooklist = book_lib.map((bk) => (
+    <Book 
+    key={bk.id} 
+    img_url={bk.img_url} 
+    name={bk.name} 
+    desc={bk.desc} 
+    price={bk.price}
+    votes={bk.votes}
+    >
+       <p>This is a line.</p>
+    </Book>
+    ));
     return(
         <div>
             <h1>Book List</h1>
